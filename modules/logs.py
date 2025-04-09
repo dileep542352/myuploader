e336fb997d7872c34290504aa96ef4dceb64f54d
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-04-09 10:32:15 +0530

    Update logs.py

--- modules/logs.py
+++ modules/logs.py
@@ -2,16 +2,13 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
-    level=logging.ERROR,
-    format=
-    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    level=logging.INFO,  # Changed to INFO to see more logs
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
-        logging.StreamHandler(),
+        logging.StreamHandler(),  # Outputs to console
     ],
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-logging = logging.getLogger()
+logger = logging.getLogger(__name__)  # Define logger here
